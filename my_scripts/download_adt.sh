beb692f1c37631dbe8fc0ce769649485ca7956ac
Esen Ergun <eergun@ucsd.edu>
2025-04-05 21:29:19 -0500

    building data store

---
+++ my_scripts/download_adt.sh
@@ -0,0 +1,4 @@
+mkdir -p checkpoints/
+wget https://dl.fbaipublicfiles.com/fairseq/models/lm/adaptive_lm_wiki103.v2.tar.bz2
+tar -xvjf adaptive_lm_wiki103.v2.tar.bz2
+rm adaptive_lm_wiki103.v2.tar.bz2
